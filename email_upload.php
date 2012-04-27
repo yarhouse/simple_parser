2a79af90d99602a0cef308fb65d315d34da4275a
Aaron Yarhouse <lopac1029@gmail.com>
2012-04-27 12:32:41 -0400

    Made uploader valid for xhtml 1.0 strict

--- email_upload.php
+++ email_upload.php
@@ -44,13 +44,27 @@ include_once 'functions.inc.php';
 	}
 	else{
 ?>
-<img src="parser.gif" /><br /><br />
-<form action="email_upload.php" method="post"
-		enctype="multipart/form-data">
-	<label for="file">Filename:</label>
-	<input type="file" name="file" id="file" /><br /><br />
-	<input type="submit" name="submit" value="Submit" />
-</form>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Parser</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<div>
+		<form action="email_upload.php" method="post" enctype="multipart/form-data">
+			<div><label for="file">Filename:</label></div>
+			<div><input type="file" name="file" id="file" /></div>
+			<div><input type="submit" name="submit" value="Submit" /></div>
+		</form>
+	</div>
+	<p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+  </p>
+</body>
+</html>
 
 <?php } ?>
 
